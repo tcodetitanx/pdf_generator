f1a5032f162e77866a3d6612ee7933930c020a3d
akbarsazish <sazishaliakbar@gmail.com>
2024-09-11 17:10:51 +0330

    Add pdf genertor page

--- generate_pdf.php
+++ generate_pdf.php
@@ -1,8 +1,8 @@
 <?php
-require_once('../invoice/tcpdf/tcpdf.php');
+// require_once('../invoice/tcpdf/tcpdf.php');
 
 // Create new PDF document
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+// $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // Set document information
 $pdf->SetCreator(PDF_CREATOR);
